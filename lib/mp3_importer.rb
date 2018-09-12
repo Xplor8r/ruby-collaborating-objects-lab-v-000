414a6825e3b3ef2bcc8f6413d5daa62003fed0a5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-12 04:31:24 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,11 +6,21 @@ class MP3Importer
   end
   
   def files
+<<<<<<< HEAD
     @files = Dir["#{path}/*.mp3"].collect {|f| f.gsub("#{path}/", "")}
   end  
   
   def import
     files.each {|f| Song.new_by_filename(f)}
+=======
+    @files = @files || Dir["#{path}/*.mp3"].collect do |filename|
+      filename.gsub("#{path}/", "")
+    end
+  end  
+  
+  def import
+    files.each { |filename| Song.new_by_filename(filename) }
+>>>>>>> da45abdba11eeb6fb4c89b0b671739ba2eb75280
   end
     
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+<<<<<<< HEAD
   attr_accessor :name, :artist
 
   def initialize(name)
@@ -18,6 +19,27 @@ class Song
     song.name = s
     song.artist_name = a
     song
+=======
+  
+  attr_accessor :name, :artist
+
+@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+    def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+>>>>>>> da45abdba11eeb6fb4c89b0b671739ba2eb75280
   end
   
 end
